05b1159ee9aff4bddd6c74df2f43094cde1adca2
Susie Coleman <susiecoleman20@gmail.com>
2019-04-28 12:31:32 +0100

    Update firebase function to handle favourite colour.

--- firebaseFulfillment/functions/src/index.ts
+++ firebaseFulfillment/functions/src/index.ts
@@ -14,4 +14,20 @@ app.intent('Welcome', conv => {
   );
 });
 
+app.intent<{ color: string }>('FavouriteColour', (conv, { color }) => {
+  conv.close(
+    `You said your favourite colour was ${color}. Here's your fortune. ${getFortune()}`
+  );
+});
+
+const getFortune = (): string => {
+  const fortunes = [
+    "You will end up negotiating Brexit. Don't worry it's not like you're going to do a worse job than others who have tried.",
+    'You will get a robot to tell you your fortune. Just like Tom Hanks in that film. Hopefully it will go better for you.',
+    'Everything will probably be fine. Or things might go badly.',
+  ];
+  const randomNumber = Math.floor(Math.random() * fortunes.length);
+  return fortunes[randomNumber];
+};
+
 exports.fortuneTeller = region('europe-west1').https.onRequest(app);
